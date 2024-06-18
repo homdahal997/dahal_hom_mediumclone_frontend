f13c90612c6f29e80da4e00ee7967ae1df88d58a
Hom Dahal <homdahal997@gmail.com>
2024-06-18 17:44:11 -0400

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer';
 import { Container, Row,Col } from 'react-bootstrap';
 import Moviecard from './components/Card';
 
@@ -19,9 +20,10 @@ function App() {
             ))}
           </Row>
         </Container>
+        
 
       </main>
-
+      <Footer />
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,28 @@
-function Footer(){
-    return <footer>This is my footer</footer>
-}
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import logo from '../images/movielogo.png'
+
+function Footer() {
+    return (
+        <footer>
+            {/* https://react-bootstrap.github.io/docs/components/navbar */}
+            <Navbar bg='' variant='light' expand='lg' collapseOnSelect className="myCustomNavbar">
+                <Container>
+                    <Navbar.Brand href='/'>
+                        <img src={logo} alt='moviemania' />
+                        MovieMania </Navbar.Brand>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse id='basic-navbar-nav'>
+                        <Nav className='ms-auto'>
+                            <Nav.Link href='/'>Home</Nav.Link>
+                            <Nav.Link href='/'>Polpular</Nav.Link>
+                            <Nav.Link href='/'>Top Rated</Nav.Link>
+                            <Nav.Link href='/'>Upcoming</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </footer>
+    );
+};
+
+export default Footer;
