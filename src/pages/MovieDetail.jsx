822c07dfa1606d882420d58602010dbd4b916a04
Hom Dahal <homdahal997@gmail.com>
2024-06-19 19:30:45 -0400

    Layout for movie detail page created

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,4 +1,13 @@
-function MovieDetail(){
-    return <main>Movie Detail Page display</main>
+import { Container, Row, Col } from 'react-bootstrap';
+function MovieDetail() {
+    return (
+        <Container>
+            <Row>
+                <Col xs={6}>xs=6</Col>
+                <Col xs={6}>xs=6</Col>
+            </Row>
+
+        </Container>
+    )
 }
 export default MovieDetail
