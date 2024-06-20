64e7f4ee1c85a39f882e986fe71995e61da14700
Hom Dahal <homdahal997@gmail.com>
2024-06-20 08:55:22 -0400

    Dark implementation using global context

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -2,13 +2,15 @@ import { fetchAPIData } from '../hooks/fetchAPIData'
 import { useTitle } from '../hooks/useTitle'
 import { Container, Row, Col } from 'react-bootstrap';
 import Moviecard from '../components/Card';
+import { useTheme } from '../contexts/ThemeContext';
 
 export const MovieList = ({ apiPath, title }) => {
+    const { isDarkMode, toggleMode } = useTheme();
     const { data: movies } = fetchAPIData(apiPath);
     useTitle(title);
 
     return (
-        <main className='py-3'>
+        <main className='py-3' >
             <Container>
                 <h1 style={{marginBottom: '50px'}}>{title}</h1>
                 <Row xs={1} md={2} lg={4} className="g-4">
