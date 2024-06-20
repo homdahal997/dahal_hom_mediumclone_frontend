6b88aebb66b1d103b59a37fc15d21e8b05810b5e
Hom Dahal <homdahal997@gmail.com>
2024-06-20 08:23:14 -0400

    Add 404 page with image and button to go back to home page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import page404 from '../images/404-Page-Not-Found.png'
 import { Container, Row, Col } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
 function PageNotFound() {
 
     useEffect(() => {
@@ -10,7 +12,16 @@ function PageNotFound() {
         <main className='py-3'>
             <Container>
                 <Row>
-                    <img style={{width : '550px', height : '550px', display: ''}} src={page404}></img>
+                <Col xs={6}> <img style={{width : '750px', height : '750px', display: ''}} src={page404}></img></Col>
+                <Col xs={6}>
+                <h3 style={{margin : '50px'}}>Sorry the page you are looking for is either moved elsewhere or completely removed from our server.</h3>
+                <div className="d-grid gap-2">
+                        <Button style={{marginBottom: '20px', marginLeft: '50px', marginTop : '100px'}}  variant="primary" size="lg">
+                        <Link style={{color:'white'}} to='/' rel="noreferrer">Go Back To Home Page</Link>
+                        </Button>
+                    </div>
+                </Col>
+                    
                 </Row>
 
             </Container>
