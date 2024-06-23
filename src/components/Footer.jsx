2118a4dad2540f337bed57194d60f0ea408e1516
Hom Dahal <homdahal997@gmail.com>
2024-06-23 13:09:23 -0400

    Update footer logo and brand name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import logo from '../images/movielogo.png'
+import logo from '../images/logocc.png'
 import { BsArrowUpCircle } from 'react-icons/bs';
 import { FaHeart } from 'react-icons/fa'; 
 
@@ -17,7 +17,7 @@ function Footer() {
                 <Container>
                     <Navbar.Brand href='/'>
                         <img src={logo} alt='moviemania' />
-                        MovieMania </Navbar.Brand>
+                        Content Craft </Navbar.Brand>
                     <Navbar.Toggle aria-controls='basic-navbar-nav' />
                     <Navbar.Collapse id='basic-navbar-nav'>
                         <Nav className='ms-auto'>
