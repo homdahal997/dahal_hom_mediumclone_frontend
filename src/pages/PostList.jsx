3dd90348803e3d87ce5221718f6d71e2bc456ef1
Hom Dahal <homdahal997@gmail.com>
2024-06-22 22:35:23 -0400

    post list component added

---
+++ src/pages/PostList.jsx
@@ -0,0 +1,29 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import { useTheme } from '../contexts/ThemeContext';
+import { fetchAPIData } from '../hooks/fetchAPIData';
+import { useTitle } from '../hooks/useTitle';
+import Post from '../components/post';
+
+export const PostList = ({ title }) => {
+    const { isDarkMode } = useTheme();
+    const { data: posts } = fetchAPIData();
+    useTitle(title);
+
+    return (
+        <main className={`py-3 ${isDarkMode ? 'dark-mode-class' : ''}`}>
+            <Container>
+                <h1 style={{ marginBottom: '50px' }}>{title}</h1>
+                <Row xs={1} md={2} lg={4} className="g-4">
+                    {posts ? posts.map((post) => (
+                        <Col sm={12} md={6} lg={4} xl={3} key={post._id}>
+                            <Post key={post._id} post={post} />
+                        </Col>
+                    )) : 
+                    <Col><p>Loading posts...</p></Col>}
+                </Row>
+            </Container>
+        </main>
+    );
+}
+
+export default PostList;
