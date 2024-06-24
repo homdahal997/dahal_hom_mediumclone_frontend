32584cbdcbf3f386533c89d1586a4fe630129e16
Hom Dahal <homdahal997@gmail.com>
2024-06-24 05:42:03 -0400

    Add delete functionality to PostList page

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,13 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { useTheme } from '../contexts/ThemeContext';
 import { useFetchPostsData } from '../hooks/fetchDataAPI';
 import { useTitle } from '../hooks/useTitle';
 import Post from '../components/post';
+import axios from 'axios';
 
 export const PostList = ({ title }) => {
     const { isDarkMode } = useTheme();
-    const { data: posts } = useFetchPostsData();
+    const fetchedData = useFetchPostsData();
+    const [posts, setPosts] = useState(fetchedData.data); 
     useTitle(title);
+
+    useEffect(() => {
+        setPosts(fetchedData.data); 
+    }, [fetchedData.data]);
+
+    // delete post
+    const deletePost = async(postId) =>{
+        try {
+            await axios.delete(`http://localhost:5050/api/v1/posts/${postId}`);
+            setPosts(currentPosts => currentPosts.filter(post => post._id !== postId));
+        } catch (error) {
+            console.error("Error deleting post:", error);
+        }
+    }
+
+    
 
     return (
         <main className={`py-3 ${isDarkMode ? 'dark-mode-class' : ''}`}>
@@ -17,6 +36,7 @@ export const PostList = ({ title }) => {
                     {posts ? posts.map((post) => (
                         <Col sm={12} md={6} lg={4} xl={3} key={post._id}>
                             <Post key={post._id} post={post} />
+                            <button onClick={() => deletePost(post._id)}>Delete</button>
                         </Col>
                     )) : 
                     <Col><p>Loading posts...</p></Col>}
