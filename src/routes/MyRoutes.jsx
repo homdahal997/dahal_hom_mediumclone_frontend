91ea3a23d45487320fce632bc2f72f0bf5f6e6cb
Hom Dahal <homdahal997@gmail.com>
2024-06-25 16:54:41 -0400

    Some route updated

--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -12,13 +12,6 @@ function MyRoutes() {
         <>
         <Routes>
             <Route path="/" element={<PostList  title="Latest Posts" />} />
-            {/* <Route path="movie/:id" element={<MovieDetail />} />
-            <Route path="movies/popular" element={<MovieList apiPath="movie/popular" title="Popular Movies"/>} />
-            <Route path="movies/top" element={<MovieList apiPath="movie/top_rated" title="Top Rated Movies" />} />
-            <Route path="movies/upcoming" element={<MovieList apiPath="movie/upcoming" title="Upcoming Movies" />} />
-            <Route path="search" element={<Search apiPath="search/movie" />} />*/}
-
-            
             <Route path="*" element={<PageNotFound title="404 Page Not Found"/>} /> 
             <Route path="/login" element={<LoginForm title="login" />} />
             <Route path="/register" element={<Register title="register" />} />
